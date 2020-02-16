abb38a2fbf7839295c9288d638fa9970b6932ca7
Andreas Rönnquist <andreas@ronnquist.net>
2020-02-16 21:37:29 +0100

    Initial commit

---
+++ po/count.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export LANG=en_CA
+
+for NAME in *.po
+do
+  echo $NAME
+  msgfmt -v $NAME -o `basename $NAME .po`.mo 2> temp
+  cat temp | awk '{ str=substr($0, match($0, /([0-9]+) translated/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' > `basename $NAME .po`.percent
+  cat temp | awk '{ str=substr($0, match($0, /([0-9]+) fuzzy/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' >> `basename $NAME .po`.percent
+  cat temp | awk '{ str=substr($0, match($0, /([0-9]+) untranslated/), RLENGTH); if(str)print str;else print 0; }' | awk '{ print $1 }' >> `basename $NAME .po`.percent
+  rm `basename $NAME .po`.mo
+done
+
+rm -f temp
