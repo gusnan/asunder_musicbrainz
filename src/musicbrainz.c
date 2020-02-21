051a208f64d179af59586e86a43b785f5263a305
Andreas Rönnquist <andreas@ronnquist.net>
2020-02-21 18:41:46 +0100

    Fix memory leak (if we get error getting the discid)

--- src/musicbrainz.c
+++ src/musicbrainz.c
@@ -36,6 +36,7 @@ char *get_discid(char *cdrom)
         fprintf(stderr, "Error: %s\n", discid_get_error_msg(discid));
         
         discid_free(discid);
+        free(result);
         return NULL;
     }
     
